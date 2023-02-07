423767dab371f2d2f5665852e000a30d8a9114df
Emmanuel <emmanuelgaboardi@gmail.com>
2023-02-07 00:20:48 -0300

    git push

--- main.js
+++ main.js
@@ -1,89 +1,77 @@
-var elem = document.querySelector('.main-carousel');
-var flkty = new Flickity( elem, {
+var elem = document.querySelector(".main-carousel");
+var flkty = new Flickity(elem, {
   // options
-  cellAlign: 'left',
+  cellAlign: "left",
   contain: true,
   autoPlay: true,
   prevNextButtons: false,
-pageDots: false
+  pageDots: false,
 });
 
 // element argument can be a selector string
 //   for an individual element
-var flkty = new Flickity( '.main-carousel', {
+var flkty = new Flickity(".main-carousel", {
   // options
-  
 });
 
 // Flickity options, defaults
 var options = {
-    prevNextButtons: false
-  };
-  
-  
-  
+  prevNextButtons: false,
+};
 
+// Responsive Menu
 
-  // Responsive Menu
+showMenu = () => {
+  let menuMobile = document.querySelector(".menu2");
+  if (menuMobile.classList.contains("open")) {
+    menuMobile.classList.remove("open");
+  } else {
+    menuMobile.classList.add("open");
+  }
+};
 
-  showMenu = () => {
-    let menuMobile = document.querySelector(".menu2");
-    if (menuMobile.classList.contains("open")) {
-      menuMobile.classList.remove("open");
-    } else {
-      menuMobile.classList.add("open");
-    }
-  };
+// UserProfile
 
-  // UserProfile
+userProfile = () => alert("Estamos em manutenção!");
 
-  userProfile = () => alert("Estamos em manutenção!");
- 
-  // Creating Rollover from the news tab
+// Creating Rollover from the news tab
 
-  const image = document.querySelector("#img1");
+const image = document.querySelector("#img1");
 
-  image.addEventListener("mouseover", function() {
-    Element.src='./IMG/gost1.png'
-  });
+image.addEventListener("mouseover", function () {
+  this.src = "./IMG/gost1.png";
+});
 
-  
+image.addEventListener("mouseout", function () {
+  this.src = "./IMG/foto1.png";
+});
 
-  image.addEventListener("mouseout", function() {
-    this.src='./IMG/foto1.png'
-  });
+const image2 = document.querySelector("#img2");
 
-  const image2 = document.querySelector("#img2");
+image2.addEventListener("mouseover", function () {
+  this.src = "./IMG/gost2.png";
+});
 
-  image2.addEventListener("mouseover", function() {
-    this.src='./IMG/gost2.png'
-  });
+image2.addEventListener("mouseout", function () {
+  this.src = "./IMG/foto2.png";
+});
 
-  
-  image2.addEventListener("mouseout", function() {
-    this.src='./IMG/foto2.png'
-  });
+const image3 = document.querySelector("#img3");
 
-  const image3 = document.querySelector("#img3");
+image3.addEventListener("mouseover", function () {
+  this.src = "./IMG/gost3.png";
+});
 
-  image3.addEventListener("mouseover", function() {
-    this.src='./IMG/gost3.png'
-  });
+image3.addEventListener("mouseout", function () {
+  this.src = "./IMG/foto3.png";
+});
 
- 
+const image4 = document.querySelector("#img4");
 
-  image3.addEventListener("mouseout", function() {
-    this.src='./IMG/foto3.png'
-  });
+image4.addEventListener("mouseover", function () {
+  this.src = "./IMG/gost4.png";
+});
 
-  const image4 = document.querySelector("#img4");
-
-  image4.addEventListener("mouseover", function() {
-    this.src='./IMG/gost4.png'
-  });
-
-  
-  image4.addEventListener("mouseout", function() {
-    this.src='./IMG/foto4.png'
-  });
-
+image4.addEventListener("mouseout", function () {
+  this.src = "./IMG/foto4.png";
+});
